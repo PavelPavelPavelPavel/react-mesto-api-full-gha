84021dda70e667f0149af419e2d2282ee8656259
Pavel <paveliva1988@gmail.com>
2023-12-27 21:23:30 +0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const path = require('path');
+
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+// const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -18,6 +24,14 @@ const app = express();
 const { checkServer } = require('./utils/responseCheck');
 
 app.use(express.json());
+app.use(function (req, res, next) {
+  const { origin } = req.headers; /
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(requestLogger);
