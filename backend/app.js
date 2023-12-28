cb028965087a2ade27e0e683c8d5da44f4a195b8
Pavel <paveliva1988@gmail.com>
2023-12-28 18:22:58 +0300

    renew url

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 
-const path = require('path');
+// const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -20,7 +20,7 @@ const { checkServer } = require('./utils/responseCheck');
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(requestLogger);
 app.use('/', appRouter);
