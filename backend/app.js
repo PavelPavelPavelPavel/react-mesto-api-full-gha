9145d0d5b850c9209021f85046f6242c50d63e98
Pavel <paveliva1988@gmail.com>
2023-12-28 18:08:18 +0300

    renew url

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(requestLogger);
-app.use('/app/', appRouter);
+app.use('/api/', appRouter);
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
