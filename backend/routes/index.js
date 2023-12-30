c57b3df8220eb9b18fce689b8b0cfd95f3e01a0e
Pavel <paveliva1988@gmail.com>
2023-12-30 15:24:50 +0300

    On check pr15

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,11 +12,11 @@ const {
   login,
 } = require('../controllers/users');
 
-// router.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Сервер сейчас упадёт');
-//   }, 0);
-// });
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.post('/signup', validateCreateUser, createUser);
 router.post('/signin', validateLogin, login);
 router.use(authUser);
